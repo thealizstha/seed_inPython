18cec64ffd4e3057ef0a07ab034ae2eaa06bd392
Aliz Shrestha <98611497+thealizstha@users.noreply.github.com>
2024-01-02 23:35:17 +0545

    seed

---
+++ gt.py
@@ -0,0 +1,6 @@
+import random
+
+random.seed(10)
+print(random.random())
+
+#the generator creates a random number based on the seed value, so if the seed value is 10, you will always get 0.5714025946899135 as the first random number.
